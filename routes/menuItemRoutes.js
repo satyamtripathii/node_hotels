8e86cd64a96a2bf1ac7a058577cde2d8ac94bade
satyamtripathii <satyamtripathi798@gmail.com>
2025-09-10 16:12:55 +0530

    menuItem name change

--- routes/menuItemRoutes.js
+++ routes/menuItemRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const MenuItem = require('./../models/menuItem');
+const MenuItem = require('../models/menuItem');
 
 router.get('/', async (req, res) => {
   try{
